2f9b29117c5df7b02b412367cc84f89da727e6dd
Kevin Traver <kevintraver@kevintraver.com>
2023-08-13 15:39:03 -0600

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  globals: {
+    google: 'readonly'
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard', 'eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {}
+}
