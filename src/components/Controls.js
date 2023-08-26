0f3d79c4dce8131881a64df06009321e647eb7ee
Kevin Traver <kevintraver@kevintraver.com>
2023-08-26 12:40:08 -0600

    add zoom feature to buttons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGlobe,
-  faMagnifyingGlassLocation
+  faMagnifyingGlassLocation,
+  faMagnifyingGlassPlus,
+  faMagnifyingGlassMinus
 } from '@fortawesome/free-solid-svg-icons'
 
 function Controls({
@@ -12,10 +15,44 @@ function Controls({
   centerMap,
   pointA,
   pointB,
-  searchLocation
+  searchLocation,
+  currentZoomLevel,
+  shouldZoomInA,
+  setShouldZoomInA,
+  shouldZoomInB,
+  setShouldZoomInB
 }) {
+  // Local state to track the previous zoom levels and icon state
+  const [prevZoomLevelA, setPrevZoomLevelA] = useState(currentZoomLevel)
+  const [prevZoomLevelB, setPrevZoomLevelB] = useState(currentZoomLevel)
+  const [iconStateA, setIconStateA] = useState(faMagnifyingGlassLocation)
+  const [iconStateB, setIconStateB] = useState(faMagnifyingGlassLocation)
+
+  // Generalized function to handle centering and zooming
+  const handleCenterAndZoom = (
+    point,
+    shouldZoomIn,
+    setShouldZoomIn,
+    setPrevZoomLevel,
+    setIconState
+  ) => {
+    if (map) {
+      centerMap(point)
+      if (shouldZoomIn) {
+        setPrevZoomLevel(currentZoomLevel)
+        map.setZoom(8) // Zoom to level 8
+        setIconState(faMagnifyingGlassMinus) // Set icon to minus
+      } else {
+        map.setZoom(prevZoomLevelA || currentZoomLevel) // Zoom to previous level or current level if null
+        setIconState(faMagnifyingGlassPlus) // Reset icon to plus
+      }
+      setShouldZoomIn(!shouldZoomIn)
+    }
+  }
+
   return (
     <>
+      {/* Existing buttons */}
       <button
         className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
         onClick={() => fitMapBoundsToPoints(map)}
@@ -23,18 +60,36 @@ function Controls({
         <FontAwesomeIcon icon={faGlobe} />
       </button>
 
+      {/* Button for point A */}
       <button
         className="absolute bottom-8 left-4 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
-        onClick={() => centerMap(pointA)}
+        onClick={() =>
+          handleCenterAndZoom(
+            pointA,
+            shouldZoomInA,
+            setShouldZoomInA,
+            setPrevZoomLevelA,
+            setIconStateA
+          )
+        }
       >
-        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
+        <FontAwesomeIcon icon={iconStateA} />
       </button>
 
+      {/* Button for point B */}
       <button
         className="absolute bottom-8 right-4 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
-        onClick={() => centerMap(pointB)}
+        onClick={() =>
+          handleCenterAndZoom(
+            pointB,
+            shouldZoomInB,
+            setShouldZoomInB,
+            setPrevZoomLevelB,
+            setIconStateB
+          )
+        }
       >
-        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
+        <FontAwesomeIcon icon={iconStateB} />
       </button>
     </>
   )
