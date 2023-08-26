7098caa7e787768b340ed444d3e2750479e47dc4
Kevin Traver <kevintraver@kevintraver.com>
2023-08-26 00:00:59 -0600

    update control icons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExpand, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGlobe,
+  faMagnifyingGlassLocation
+} from '@fortawesome/free-solid-svg-icons'
 
 function Controls({
   fitBoundsToPoints,
@@ -17,33 +20,28 @@ function Controls({
         className="absolute bottom-32 right-2 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
         onClick={() => fitBoundsToPoints(map)}
       >
-        <FontAwesomeIcon icon={faExpand} />
+        <FontAwesomeIcon icon={faGlobe} />
       </button>
 
-      {/* New Button to Center on pointA */}
       <button
         className="absolute bottom-24 right-2 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
         onClick={() => centerMap(pointA)}
       >
-        <FontAwesomeIcon icon={faMapMarkerAlt} />
-        Point A
+        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />A
       </button>
 
-      {/* New Button to Center on pointB */}
       <button
         className="absolute bottom-16 right-2 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
         onClick={() => centerMap(pointB)}
       >
-        <FontAwesomeIcon icon={faMapMarkerAlt} />
-        Point B
+        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />B
       </button>
 
-      {/* New Button to Center on searchLocation */}
       <button
         className="absolute bottom-8 right-2 z-10 bg-white px-4 h-8 flex items-center border border-gray-300 rounded cursor-pointer"
         onClick={() => centerMap(searchLocation)}
       >
-        <FontAwesomeIcon icon={faMapMarkerAlt} />
+        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
         Search Location
       </button>
     </>
